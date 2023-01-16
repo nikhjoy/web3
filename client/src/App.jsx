1d40fb4f826684666ba6fc2e713022e662f38a28
Nikhil Joy <nikh.liberty77@gmail.com>
2023-01-17 00:45:36 +0530

    Add existing project files to Git

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+export { Navbar, Welcome, Footer, Services, Transactions, Loader}  from './Components';
+
+const App = () => {
+
+
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+        <Services />
+        <Transactions />
+        <Footer />
+    </div>
+  );
+}
+
+export default App
---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as loader } from './Loader';
+export { default as loader } from './Navbar';
+export { default as loader } from './Footer';
+export { default as loader } from './Services';
+export { default as loader } from './Transactions';
+export { default as loader } from './Welcome';
